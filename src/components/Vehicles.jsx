fc2086714e2c6b2e6a42d4162aef835eb892709d
Andreea Teodora Panut <atp@greenmobility.com>
2021-09-16 18:29:22 +0200

    Function closed

--- src/components/Vehicles.jsx
+++ src/components/Vehicles.jsx
@@ -1,28 +1,16 @@
 import React, { useState } from "react";
 
-type Cars = {
-  id: string,
-  typeOfCar: string,
-  carNumber: string,
-  fuelMin: number,
-  fuelMax: number,
-  location: string,
-};
-
 export const VehicleInfo = (props) => {
   return (
     <div className="Car-data">
-      {props.cars.map((car)=> {
+      {props.cars.map((car) => {
         return (
-          <div>
-            <p>
-              {car.id}, {car.typeOfCar}, {car.carNumber}, {car.fuelMin},{" "}
-              {car.fuelMax}, {car.location}
-            </p>
-          </div> )
-      }
-
-      )
-  </div> 
-
-    }
+          <p>
+            {car.id}, {car.typeOfCar}, {car.carNumber}, {car.fuelMin},
+            {car.fuelMax}, {car.location}
+          </p>
+        );
+      })}
+    </div>
+  );
+};
